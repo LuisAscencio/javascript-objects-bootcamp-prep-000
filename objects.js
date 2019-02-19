9f8bbae8ba72171d5828e3857fa32986d8328516
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-19 19:08:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ var playlist ={ "Blur": "Coffe ands TV", "Cerati": "Puente" }
 function updatePlaylist(playlist,artistNames, songtitles){
 playlist[artistNames]= songtitles
 }
+
+
+function updatePlaylist(playlist,artistNames, songtitles){
+playlist[artistNames]= songtitles
+}
